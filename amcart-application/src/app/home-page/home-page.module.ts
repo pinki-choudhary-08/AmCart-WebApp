dd0abbaeda591214d35b3559b2d459daf8dbba2a
Pinki Choudhary <pchoudhary@lutron.com>
2021-01-02 18:57:39 +0530

    Few pages added

--- amcart-application/src/app/home-page/home-page.module.ts
+++ amcart-application/src/app/home-page/home-page.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { SharedModule } from '../shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomePageComponent],
   imports: [
+    SharedModule,
+    CommonModule
+  ],
+  exports: [
+    SharedModule,
     CommonModule
   ]
 })
