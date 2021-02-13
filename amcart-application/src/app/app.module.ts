aee05a946ef119e9ce63f9253cf4ad43ad985955
Rakes Manna <rakes.manna@nagarro.com>
2021-02-13 10:35:45 +0530

    order checkout component added

--- amcart-application/src/app/app.module.ts
+++ amcart-application/src/app/app.module.ts
@@ -8,12 +8,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductCategoryComponent } from './product/product-category/product-category.component';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { ShoppingCartComponent } from './product/shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './order/checkout/checkout.component';
+import { PaymentComponent } from './order/payment/payment.component';
+import { AddressComponent } from './order/address/address.component';
+import { OrderPlacedComponent } from './order/order-placed/order-placed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductCategoryComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    CheckoutComponent,
+    PaymentComponent,
+    AddressComponent,
+    OrderPlacedComponent
   ],
   imports: [
     BrowserModule,
