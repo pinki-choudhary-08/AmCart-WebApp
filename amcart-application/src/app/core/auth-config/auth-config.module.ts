959a96cfa5eaa2db697334b47c48c7bb72e7cfe7
prgupta <prgupta@lutron.com>
2021-02-15 17:28:23 +0530

    revert port changes

--- amcart-application/src/app/core/auth-config/auth-config.module.ts
+++ amcart-application/src/app/core/auth-config/auth-config.module.ts
@@ -7,14 +7,14 @@ export function loadConfig(oidcConfigService: OidcConfigService) {
       stsServer: 'https://topnotchprod.b2clogin.com/topnotchprod.onmicrosoft.com/B2C_1_susi_social_idp/v2.0',
       authWellknownEndpoint:
         'https://topnotchprod.b2clogin.com/topnotchprod.onmicrosoft.com/B2C_1_susi_social_idp/v2.0/.well-known/openid-configuration',
-      redirectUrl: "http://localhost:6420",
-      postLogoutRedirectUri: "http://localhost:6420",
+      redirectUrl: "http://localhost:4200",
+      postLogoutRedirectUri: "http://localhost:4200",
       clientId: 'c53d58ef-169e-4d9e-aaa6-3b76e53c069a',
       scope: 'openid profile https://topnotchprod.onmicrosoft.com/backend-function-id/FullAccess',
       responseType: 'code',
       silentRenew: true,
       autoUserinfo: false,
-      silentRenewUrl: "http://localhost:6420",
+      silentRenewUrl: "http://localhost:4200",
       renewTimeBeforeTokenExpiresInSeconds: 60,
       // useRefreshToken: true, // for refresh renew, but revocation and one time usage is missing from server impl.
       // ignoreNonceAfterRefresh: true,
