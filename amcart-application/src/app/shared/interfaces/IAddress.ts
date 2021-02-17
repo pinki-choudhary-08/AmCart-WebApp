c493dab2b7adf80137b8b683a7776cee18402df7
prgupta <prgupta@lutron.com>
2021-02-17 18:49:12 +0530

    address added

---
+++ amcart-application/src/app/shared/interfaces/IAddress.ts
@@ -0,0 +1,15 @@
+// Interface for product.
+export interface IAddress {
+    id: string;
+    CustomerName: string;
+    CustomerId: string;
+    addressLine1: string;
+    addressLine2: string;
+    landmark: string;
+    city: string;
+    state: string;
+    pincode: string;
+    isDefault: string;
+    contactNumber: string;
+    country: string;
+}
